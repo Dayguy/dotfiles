a7c0948a222cdd24d2033df5021b5a2a020320ff
Shawn Day <shawn.day@gmail.com>
2024-09-10 13:26:55 -0400

    Adding a few dot files from current Arch laptop

---
+++ aliases.zsh
@@ -0,0 +1,18 @@
+# Alias definitions for zsh
+# Shawn Day 2024.08.25
+
+# ls output
+alias la='ls -AlhF --color=auto'
+alias lt='ls -Altr --color=auto'
+alias cla='clear;ls -AlhF --color=auto'
+alias clt='clear;ls -Altr --color=auto'
+
+# cd behavior
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+
+#fastfetch
+alias ff='fastfetch'
+alias neofetch='fastfetch'
